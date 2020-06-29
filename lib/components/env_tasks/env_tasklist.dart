6e1d9820ea36e9576131f4d5eab1d57eb833ac2b
Yashasvini <mail.yashasvini@gmail.com>
2020-06-29 11:21:23 +0530

    added buttons and tasks

---
+++ lib/components/env_tasks/env_tasklist.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class EnvTaskList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<>();
+  }
+}
---
+++ lib/components/env_tasks/environmental_tasks.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:hack20flutter/screens/tabs/Environmental.dart';
+
+class EnvironmentalTask extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
